fd9863a6868a2f13195c0263a6ff6d2615dd082e
Exempti0n <sj990927@gmail.com>
2022-09-10 00:47:34 +0900

    Initial Commit

---
+++ BloodPressure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloodPressure
+//
+//  Created by 오승준 on 2022/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
