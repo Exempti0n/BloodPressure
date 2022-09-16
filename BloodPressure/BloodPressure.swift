30ed5e2cf7237dfa2e673b5b63060d9a2f941545
Exempti0n <sj990927@gmail.com>
2022-09-17 03:22:55 +0900

    UI create

---
+++ BloodPressure/BloodPressure.swift
@@ -0,0 +1,14 @@
+//
+//  BloodPressure.swift
+//  BloodPressure
+//
+//  Created by 오승준 on 2022/09/10.
+//
+
+import Foundation
+struct BloodPressure {
+    var Systolic: Int // 수축기
+    var Diastolic: Int //이완기
+    var Pulse: Int //맥박
+    var Date: Date //날짜
+}
---
+++ BloodPressure/BloodPressureCell.swift
@@ -0,0 +1,17 @@
+//
+//  BloodPressureCell.swift
+//  BloodPressure
+//
+//  Created by 오승준 on 2022/09/10.
+//
+
+import UIKit
+
+class BloodPressureCell: UICollectionViewCell {
+    @IBOutlet weak var DateLabel: UILabel!
+    @IBOutlet weak var SystolicLabel: UILabel!
+    @IBOutlet weak var DiastolicLabel: UILabel!
+    @IBOutlet weak var PulseLabel: UILabel!
+    
+    
+}
